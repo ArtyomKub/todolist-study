7927185b8bf40f836553c7334dc3f3c9691348d2
ArtyomKub <kub.killjoy@gmail.com>
2023-01-12 14:00:05 +0300

    Remove tasks number 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import {Todolist} from "./Todolist";
 
 const App = () => {
 
-    const shapka = 'What to learn'
-
     const tasks1 = [
         {id: 1, title: 'HTML $ CSS', isDone: true},
         {id: 2, title: 'JavaScript', isDone: true},
@@ -13,16 +11,10 @@ const App = () => {
         {id: 4, title: 'ReactJS', isDone: false},
         {id: 5, title: 'ReactJS', isDone: false}
     ]
-    const tasks2 = [
-        {id: 1, title: 'Hello World', isDone: true},
-        {id: 2, title: 'I am happy', isDone: false},
-        {id: 3, title: 'Fuck', isDone: false}
-    ]
 
     return (
         <div className="App">
-            <Todolist title={shapka} tasks={tasks1}/>
-            <Todolist title={'What to learn 2222'} tasks={tasks2}/>
+            <Todolist title={'What to learn'} tasks={tasks1}/>
         </div>
     );
 }
